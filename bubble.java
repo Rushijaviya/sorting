2462219037ab05f99f4e43cb2a3a9966b31b24d9
Rushi Javiya <59735978+Rushijaviya@users.noreply.github.com>
2020-09-23 23:14:08 +0530

    sorting using bubble sort

---
+++ bubble.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class bubble {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        System.out.println("Enter length");
+        n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter elements");
+        for(i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++){
+            for(j=0;j<n-i-1;j++){
+                if(a[j]>a[j+1]){
+                    int temp=a[j+1];
+                    a[j+1]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        for(i=0;i<n;i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+}
