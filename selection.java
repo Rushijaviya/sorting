33315f9850e88dc90160a1445f5c9cf9d35449a3
manthan <68530736+manthanghasadiya@users.noreply.github.com>
2020-10-12 19:01:36 +0530

    Added algorithm for Selection Sort
    Added algorithm and Changed some of print lines for a good looking and user friendly output.

--- selection.java
+++ selection.java
@@ -1,15 +1,29 @@
+/*
+ * The selection sort algorithm sorts an array by repeatedly finding the minimum element from unsorted part and putting it at the beginning. 
+ * The algorithm maintains two subarrays in a given array.
+ * 
+ * 1) The subarray which is already sorted.
+ * 2) Remaining subarray which is unsorted.
+ * 
+ * In every iteration of selection sort, the minimum element from the unsorted subarray is picked and moved to the sorted subarray.
+*/
 import java.util.*;
 public class selection {
     public static void main(String args[]){
         Scanner hp=new Scanner(System.in);
         int n,i,j;
-        System.out.println("Enter length of array");
+        System.out.println("Enter length of array: ");
         n=hp.nextInt();
         int a[]=new int[n];
-        System.out.println("Enter elements of array");
+        System.out.println("Enter elements of array: ");
         for(i=0;i<n;i++){
             a[i]=hp.nextInt();
         }
+        System.out.print("Given Array is: "+"[");
+        for(i=0;i<n;i++) {
+        	System.out.print(a[i] + " ");
+        }
+        System.out.print("]");
         for(i=0;i<n-1;i++){
             int min=i;
             for(j=i+1;j<n;j++){
@@ -20,9 +34,11 @@ public class selection {
             a[i]=a[min];
             a[min]=temp;
         }
+        System.out.print("\nSorted  Array is: "+"[");
         for(i=0;i<n;i++){
             System.out.print(a[i] + " ");
         }
+        System.out.print("]");
     }
 }
 /*
